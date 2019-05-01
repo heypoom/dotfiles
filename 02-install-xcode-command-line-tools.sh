5befa16f5ff95a3697fb304c4ce63a2ee040933e
Phoomparin Mano <phoomparin@gmail.com>
2019-05-01 16:58:17 +0700

    :wrench:

--- 02-install-xcode-command-line-tools.sh
+++ 02-install-xcode-command-line-tools.sh
@@ -3,6 +3,9 @@
 # Install XCode
 xcode-select --install
 
+# Switch to the correct xcode developer tools path
+sudo xcode-select --switch /Applications/Xcode.app/Contents/Developer
+
 # Agree to XCode Build License
 sudo xcodebuild -license
 
---
+++ 18-install-gem-dependencies.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+gem install pry bundler docker-sync dotenv irbtools rake rcodetools rubocop tmuxinator
