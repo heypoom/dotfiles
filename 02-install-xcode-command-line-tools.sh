38509e20d0d52917d62bc00d17141d3447f9f00b
Phoomparin Mano <phoomparin@gmail.com>
2019-05-01 11:55:26 +0700

    :recycle: Install C/C++ headers when installing XCode CLI tools

--- 02-install-xcode-command-line-tools.sh
+++ 02-install-xcode-command-line-tools.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
 xcode-select --install
+
+# Install C/C++ Headers for macOS
+open "/Library/Developer/CommandLineTools/Packages/macOS_SDK_headers_for_macOS_10.14.pkg"
--- 13-install-clang-headers.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-open "/Library/Developer/CommandLineTools/Packages/macOS_SDK_headers_for_macOS_10.14.pkg"
