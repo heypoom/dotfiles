1dbfd6807d51f978818333e551be9f57e6629848
Phoomparin Mano <phoomparin@gmail.com>
2019-05-04 23:34:41 +0700

    :sparkles: Add IPython configuration

--- 00-init-and-link.sh
+++ 00-init-and-link.sh
@@ -15,9 +15,23 @@ link_file "git/gitconfig" ~/.gitconfig
 link_file "tmux/tmux.conf" ~/.tmux.conf
 link_file "tmux/tmuxline.tmux.conf" ~/.tmuxline.tmux.conf
 
+# Link IPython configuration
+link_file "ipython" ~/.ipython
+
 # Link .ideavimrc
 link_file "ideavim/ideavimrc" ~/.ideavimrc
 
 # Link SSH config
 mkdir -p ~/.ssh
 link_file "ssh/config" ~/.ssh/config
+
+# Link pygments themes for IPython
+function link_pygments_theme {
+  echo "Linking Pygments Theme: $1"
+
+  link_file "ipython/profile_default/$1.py" "/usr/local/lib/python3.7/site-packages/pygments/styles/"
+}
+
+link_pygments_theme awesome
+link_pygments_theme dracula
+
