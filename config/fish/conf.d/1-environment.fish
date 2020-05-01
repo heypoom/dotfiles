a34499c2ac7285dae12c7374de0075d2422fca80
Phoomparin Mano <phoomparin@gmail.com>
2020-05-01 20:11:13 +0700

    :bug: Use proper syntax to add the environment variables

--- config/fish/conf.d/1-environment.fish
+++ config/fish/conf.d/1-environment.fish
@@ -56,3 +56,37 @@ set -gx PKG_CONFIG_PATH "/usr/local/opt/openssl/lib/pkgconfig:/usr/local/opt/lib
 
 # Define Java's SDK Path
 set -gx JAVA_HOME '/Users/phoomparin/.asdf/installs/java/oracle-8.141'
+
+# Add GNU manuals to the MANPATH
+set -gx MANPATH /usr/local/opt/coreutils/libexec/gnuman/ /usr/share/man $MANPATH
+
+# Configure the environment variables.
+set -gx PATH \
+  /usr/local/bin \
+  /usr/bin \
+  /usr/local/sbin \
+  /usr/libexec \
+  $GEM_HOME/.gem/bin \
+  $HOME/Scripts \
+  /usr/local/opt/llvm/bin \
+  /usr/local/opt/coreutils/libexec/gnubin \
+  $ANDROID_HOME/tools \
+  $ANDROID_HOME/platform-tools \
+  $HOME/.fastlane/bin \
+  $HOME/.cargo/bin \
+  $HOME/lib/gcloud/bin \
+  $HOME/.config/yarn/global/node_modules/.bin \
+  $HOME/.pyenv/bin \
+  $HOME/.poetry/bin \
+  $HOME/lib/flutter/bin \
+  /usr/local/opt/dart@2/bin \
+  $HOME/Library/Python/3.7/bin \
+  $GOPATH/bin \
+  $HOME/bin \
+  $HOME/.local/bin \
+  $HOME/.platformio/penv/bin \
+  $HOME/lib/emsdk/emscripten/1.37.34/bin \
+  $HOME/.stack/programs/x86_64-osx/ghc-8.6.4/bin \
+  $HOME/.asdf/bin \
+  $HOME/.asdf/shims \
+  $PATH
