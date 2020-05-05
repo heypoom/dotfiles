b5e596ebd54d2a9d52ae573743a3b57e7bda7f29
Phoomparin Mano <phoomparin@gmail.com>
2020-05-05 16:58:02 +0700

    :zap: Remove redundant configuration for tmux performance

--- config/fish/config.fish
+++
@@ -1,7 +0,0 @@
-# iTerm Integration
-# if test "$TERM_PROGRAM" = iTerm.app
-# 	set -gx iterm2_hostname "phoomparin.local"
-# 	set -gx ITERM_ENABLE_SHELL_INTEGRATION_WITH_TMUX true
-
-#   test -e {$HOME}/.iterm2_shell_integration.fish && source {$HOME}/.iterm2_shell_integration.fish
-# end
