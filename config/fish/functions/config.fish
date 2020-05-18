4bb663a5ddc96f5b31dff44d7810c4601e4ca415
Phoomparin Mano <phoomparin@gmail.com>
2020-05-18 15:18:47 +0700

    :wrench: Update skhd hotkey for yabai wm

--- config/fish/functions/config.fish
+++ config/fish/functions/config.fish
@@ -1,4 +1,4 @@
-# Defined in /var/folders/jm/l3mwrkrx7yq8nhxyg1btdzkm0000gn/T//fish.wZbDLb/config.fish @ line 2
+# Defined in /var/folders/jm/l3mwrkrx7yq8nhxyg1btdzkm0000gn/T//fish.4qM6tp/config.fish @ line 2
 function config
 	switch $argv[1]
 		case yabai
@@ -56,6 +56,10 @@ function config
 		case status
 			pushd "$HOME/.dotfiles"
 			git status
+			popd
+		case diff
+			pushd "$HOME/.dotfiles"
+			git diff HEAD
 			popd
 		case reset
 			pushd "$HOME/.dotfiles"
