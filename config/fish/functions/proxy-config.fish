8603105957619067ed7bbd060dd76fdc5159efb7
Phoomparin Mano <phoomparin@gmail.com>
2020-05-31 14:43:33 +0700

    :bug: Show host and port of proxy

--- config/fish/functions/proxy-config.fish
+++ config/fish/functions/proxy-config.fish
@@ -1,10 +1,10 @@
-# Defined in /var/folders/jm/l3mwrkrx7yq8nhxyg1btdzkm0000gn/T//fish.MKERIl/proxy-config.fish @ line 1
+# Defined in /var/folders/jm/l3mwrkrx7yq8nhxyg1btdzkm0000gn/T//fish.2nHuFH/proxy-config.fish @ line 2
 function proxy-config
 	set adapter "Wi-Fi"
 	set host argv[1]
 	set port argv[2]
 
-	printf "Proxying to %s:%s" host port
+	printf "Proxying to %s:%s" $host $port
 
 	networksetup -setwebproxy $adapter $host $port
 	networksetup -setwebproxystate $adapter on
