fac1f9dbe496f0bd5fd249cc7ed9c3a7965e4fba
Phoomparin Mano <phoomparin@gmail.com>
2020-06-07 14:22:26 +0700

    :sparkles: Add macOS VSCode settings to dotfiles

--- config/fish/functions/gi.fish
+++ config/fish/functions/gi.fish
@@ -1,5 +1,5 @@
-# Gitignore CLI
+# Defined in /var/folders/jm/l3mwrkrx7yq8nhxyg1btdzkm0000gn/T//fish.nmPbse/gi.fish @ line 2
 function gi --description 'gitignore.io cli for fish'
   set -l params (echo $argv|tr ' ' ',')
-  curl -s https://www.gitignore.io/api/$params
+  curl -s https://www.toptal.com/developers/gitignore/api/$params
 end
