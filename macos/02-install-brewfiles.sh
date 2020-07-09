4484c8e4396e1c1868cc5e6df316f08ed6b05d06
Phoomparin Mano <phoomparin@gmail.com>
2020-07-10 01:05:27 +0700

    :sparkles: Use symlinked brewfile by default

--- macos/02-install-brewfiles.sh
+++ macos/02-install-brewfiles.sh
@@ -2,17 +2,26 @@
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-source ../utils/install-brewfile.sh
+if [ "$USE_MODULAR_BREWFILE" = true ]
+then
+  source ../utils/install-brewfile.sh
 
-# sudo chown -R $(whoami) /usr/local/share/man/man8
+  sudo chown -R $(whoami) /usr/local/share/man/man8
 
-install-brewfile "Browsers" "/Applications/Google Chrome.app"
-install-brewfile "Terminals" "/Applications/Kitty.app"
-install-brewfile "Essentials" "/Applications/Keybase.app"
-install-brewfile "Development" "/Applications/Docker.app"
-install-brewfile "Productivity" "/Applications/Alfred 4.app"
-install-brewfile "Fonts" "$HOME/Library/Fonts/JetBrainsMono-Regular.ttf"
+  install-brewfile "Browsers" "/Applications/Google Chrome.app"
+  install-brewfile "Terminals" "/Applications/Kitty.app"
+  install-brewfile "Essentials" "/Applications/Keybase.app"
+  install-brewfile "Development" "/Applications/Docker.app"
+  install-brewfile "Productivity" "/Applications/Alfred 4.app"
+  install-brewfile "Fonts" "$HOME/Library/Fonts/JetBrainsMono-Regular.ttf"
 
-install-brewfile-cli "Essentials" "exa"
-install-brewfile-cli "Extras" "tcpdump"
+  install-brewfile-cli "Essentials" "exa"
+  install-brewfile-cli "Extras" "tcpdump"
+  
+  exit 0
+fi
+
+echo "📦 Installing from brewfile..."
+brew bundle --global
+echo "✅ Installed brewfile!"
 
