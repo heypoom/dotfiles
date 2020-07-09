48f8e08c9a148e9d6ff486376a6673790c520847
Phoomparin Mano <phoomparin@gmail.com>
2020-07-10 00:53:59 +0700

    :wrench: Add `config karabiner` command

--- config/fish/functions/config.fish
+++ config/fish/functions/config.fish
@@ -1,4 +1,4 @@
-# Defined in /var/folders/53/f2kch9hd5rxbkzjr4p9cg90w0000gn/T//fish.vv624o/config.fish @ line 2
+# Defined in /var/folders/53/f2kch9hd5rxbkzjr4p9cg90w0000gn/T//fish.7mgPW8/config.fish @ line 2
 function config
 	function config_edit_file
 		$EDITOR "$HOME/"$argv[1]
@@ -82,6 +82,9 @@ function config
 			config_edit_as_json Notes/.obsidian/config
 		case obsidian-css
 			config_edit_file Notes/obsidian.css
+		case karabiner
+			config_edit_file .config/karabiner.edn
+			goku
 		case commit
 			pushd "$HOME/dotfiles"
 			git add --all
