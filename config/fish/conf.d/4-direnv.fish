90e1370da13964f0787bc6ca9bba765f5fa2f12a
Phoomparin Mano <poom@hey.com>
2021-05-24 21:18:34 +0700

    chore: handle multiple direnv bin

--- config/fish/conf.d/4-direnv.fish
+++ config/fish/conf.d/4-direnv.fish
@@ -1,6 +1,6 @@
-set -x DIRENV_BIN (which -a direnv)
+set -x DIRENV_BIN (which direnv)
 
-if test -e $DIRENV_BIN
+if test -e "$DIRENV_BIN"
 	function __direnv_export_eval --on-event fish_postexec;
 		"$DIRENV_BIN" export fish | source;
 	end
