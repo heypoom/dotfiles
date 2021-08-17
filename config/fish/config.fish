0e4ae9d1c12fabf78bbb72483fc5975fe22b9c28
Phoomparin Mano <poom@hey.com>
2021-08-17 10:51:12 +0700

    chore: configure volta

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,9 +1,11 @@
-
-
 # Wasmer
 export WASMER_DIR="/Users/phoomparin/.wasmer"
 [ -s "$WASMER_DIR/wasmer.sh" ] && source "$WASMER_DIR/wasmer.sh"
 
+# Wasmtime
 set -gx WASMTIME_HOME "$HOME/.wasmtime"
-
 string match -r ".wasmtime" "$PATH" > /dev/null; or set -gx PATH "$WASMTIME_HOME/bin" $PATH
+
+# Volta
+set -gx VOLTA_HOME "$HOME/.volta"
+set -gx PATH "$VOLTA_HOME/bin" $PATH
