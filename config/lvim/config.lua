af15e0b31dde9ebb4e959d3ba1553dba5040263a
Phoomparin Mano <poom@hey.com>
2021-12-06 00:38:42 +0700

    chore: add dap

--- config/lvim/config.lua
+++ config/lvim/config.lua
@@ -61,8 +61,11 @@ lvim.builtin.which_key.mappings["t"] = {
 
 -- TODO: User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
+lvim.builtin.lualine.active = true
+lvim.builtin.dap.active = true
 lvim.builtin.dashboard.active = true
 lvim.builtin.terminal.active = true
+lvim.builtin.bufferline.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.show_icons.git = 0
 
