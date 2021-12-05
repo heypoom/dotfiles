06dfc12631e1e7cceedf64cb0e341bb9fa2a755f
Phoomparin Mano <poom@hey.com>
2021-12-05 21:09:54 +0700

    chore: install by default

--- common/08-setup-lunarvim.sh
+++ common/08-setup-lunarvim.sh
@@ -11,7 +11,7 @@ echo "Installing LunarVim (https://www.lunarvim.org)"
 curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh | tee -a install-lunarvim.sh
 chmod +x install-lunarvim.sh
 
-./install-lunarvim.sh --no-install-dependencies
+./install-lunarvim.sh --install-dependencies
 
 rm install-lunarvim.sh
 
