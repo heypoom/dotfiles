cafbb9922625ce7c1fdcfa96a30b200c9b87e2f6
Phoomparin Mano <poom@hey.com>
2021-12-05 20:41:08 +0700

    fix: skip if LunarVim is already installed

--- common/08-setup-lunarvim.sh
+++ common/08-setup-lunarvim.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+if [ -d "$HOME/.local/share/lunarvim" ]
+then
+  echo "LunarVim is already installed! Skipping."
+  exit 0
+fi
+
 echo "Installing LunarVim (https://www.lunarvim.org)"
 
 curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh | tee -a install-lunarvim.sh
