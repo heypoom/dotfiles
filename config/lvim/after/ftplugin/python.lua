7ab5c2b569e5967851470b78f7e09bed31551ace
Phoomparin Mano <poom@hey.com>
2021-12-06 11:59:37 +0700

    feat: lua formatter

--- config/lvim/after/ftplugin/python.lua
+++ config/lvim/after/ftplugin/python.lua
@@ -1,7 +1,6 @@
-local formatters = require "lvim.lsp.null-ls.formatters"
+local formatters = require("lvim.lsp.null-ls.formatters")
 
-formatters.setup {
-  { exe = "black", filetypes = { "python" } },
-  { exe = "isort", filetypes = { "python" } },
-}
-
+formatters.setup({
+	{ exe = "black", filetypes = { "python" } },
+	{ exe = "isort", filetypes = { "python" } },
+})
--- config/lvim/after/ftplugin/sh.lua
+++ config/lvim/after/ftplugin/sh.lua
@@ -1,13 +1,12 @@
-local linters = require "lvim.lsp.null-ls.linters"
+local linters = require("lvim.lsp.null-ls.linters")
 
-linters.setup {
-  {
-    exe = "shellcheck",
-    filetypes = {"sh"},
+linters.setup({
+	{
+		exe = "shellcheck",
+		filetypes = { "sh" },
 
-    ---@usage arguments to pass to the formatter
-    -- these cannot contain whitespaces.
-    args = { "--severity", "warning" },
-  },
-}
-
+		---@usage arguments to pass to the formatter
+		-- these cannot contain whitespaces.
+		args = { "--severity", "warning" },
+	},
+})
