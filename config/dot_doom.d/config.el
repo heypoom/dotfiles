56aaa2254704b1d8b66cd49f0098db558dc60d4b
Phoomparin Mano <poom@hey.com>
2021-12-11 00:32:18 +0700

    chore: comment out emacs config

--- config/dot_doom.d/config.el
+++ config/dot_doom.d/config.el
@@ -19,9 +19,6 @@
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
 
-;; (setq doom-font (font-spec :family "JetBrains Mono Nerd Font" :size 16 :weight 'semi-light)
-;;       doom-variable-pitch-font (font-spec :family "sans" :size 16))
-
 (setq doom-font (font-spec :family "JetBrainsMono Nerd Font" :size 16))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
