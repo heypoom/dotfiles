5cba00b6985f054b640b5ec5466ae1895f3bc896
Phoomparin Mano <poom@hey.com>
2021-12-11 12:33:15 +0700

    fix: only use nodejs from asdf

--- setup/linux/02-install-extra-deps.sh
+++ setup/linux/02-install-extra-deps.sh
@@ -1,18 +1,5 @@
 #!/usr/bin/env bash
 
-echo "⚪️ installing additional dependencies..."
-
-# Install Node.js for the LTS version.
-echo "⚪ installing node"
-curl -sL https://deb.nodesource.com/setup_lts.x | sudo bash -
-
-# Install Global Node Packages
-echo "⚪ installing global node packages"
-
-# Install diff-so-fancy via yarn (needed for git diff views)
-export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
-npm i -g diff-so-fancy
-
 # Install essential linux dependencies when CODESPACES_MODE is true.
 # Do not run when in dockerized container, since that repeats the cached dependency setup.
 if [[ $CODESPACES_MODE && ! $DOCKERIZED ]]
