9be5e970630e115d0f1480403197841fab014eeb
Phoomparin Mano <phoomparin@gmail.com>
2023-05-29 22:42:45 +0700

    chore: nvchad custom config

--- config/dot_config/nvim/lua/custom/chadrc.lua
+++ config/dot_config/nvim/lua/custom/chadrc.lua
@@ -1,9 +1,20 @@
--- First read our docs (completely) then check the example_config repo
-
+---@type ChadrcConfig
 local M = {}
+
+-- Path to overriding theme and highlights files
+local highlights = require "custom.highlights"
 
 M.ui = {
   theme = "catppuccin",
+  theme_toggle = { "catppuccin", "chadracula" },
+
+  hl_override = highlights.override,
+  hl_add = highlights.add,
 }
+
+M.plugins = "custom.plugins"
+
+-- check core.mappings for table structure
+M.mappings = require "custom.mappings"
 
 return M
--- config/dot_config/nvim/lua/custom/init.lua
+++ config/dot_config/nvim/lua/custom/init.lua
@@ -1,1 +1,7 @@
--- init file for NVChad
+-- local autocmd = vim.api.nvim_create_autocmd
+
+-- Auto resize panes when resizing nvim window
+-- autocmd("VimResized", {
+--   pattern = "*",
+--   command = "tabdo wincmd =",
+-- })
