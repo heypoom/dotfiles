064f3d9d2136bd60be6a1876a66d032bc3615b2d
Phoomparin Mano <phoomparin@gmail.com>
2023-05-29 22:47:51 +0700

    chore: lunarvim

--- config/dot_config/lvim/config.lua
+++ config/dot_config/lvim/config.lua
@@ -18,16 +18,18 @@ lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 -- lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
 -- lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
 
--- -- Use which-key to add extra bindings with the leader-key prefix
+-- Use which-key to add extra bindings with the leader-key prefix
 -- lvim.builtin.which_key.mappings["W"] = { "<cmd>noautocmd w<cr>", "Save without formatting" }
 -- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
 
--- -- Change theme settings
--- lvim.colorscheme = "lunar"
+-- Change theme settings
+-- lvim.colorscheme = "catppuccin"
+lvim.colorscheme = "dracula"
 
-lvim.builtin.alpha.active = true
-lvim.builtin.alpha.mode = "dashboard"
+lvim.builtin.alpha.active = false
+
 lvim.builtin.terminal.active = true
+
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
 
@@ -102,7 +104,6 @@ lvim.builtin.treesitter.auto_install = true
 -- })
 
 -- General Settings
-lvim.colorscheme = "lunar"
 lvim.transparent_window = true
 vim.opt.spell = false
 
@@ -128,39 +129,39 @@ local _, actions = pcall(require, "telescope.actions")
 lvim.builtin.treesitter.rainbow.enable = true
 
 lvim.builtin.telescope.defaults.mappings = {
-	-- Input Mode
-	i = {
-		["<C-j>"] = actions.move_selection_next,
-		["<C-k>"] = actions.move_selection_previous,
-		["<C-n>"] = actions.cycle_history_next,
-		["<C-p>"] = actions.cycle_history_prev,
-	},
+  -- Input Mode
+  i = {
+    ["<C-j>"] = actions.move_selection_next,
+    ["<C-k>"] = actions.move_selection_previous,
+    ["<C-n>"] = actions.cycle_history_next,
+    ["<C-p>"] = actions.cycle_history_prev,
+  },
 
-	-- Normal Mode
-	n = {
-		["<C-j>"] = actions.move_selection_next,
-		["<C-k>"] = actions.move_selection_previous,
-	},
+  -- Normal Mode
+  n = {
+    ["<C-j>"] = actions.move_selection_next,
+    ["<C-k>"] = actions.move_selection_previous,
+  },
 }
 
 -- Use which-key to add extra bindings with the leader-key prefix
 lvim.builtin.which_key.mappings["P"] = {
-	name = "Projects",
+  name = "Projects",
 
-	p = { "<cmd>Telescope project<CR>", "Project" },
-	r = { "<cmd>Telescope projects<CR>", "Recent" },
+  p = { "<cmd>Telescope project<CR>", "Project" },
+  r = { "<cmd>Telescope projects<CR>", "Recent" },
 }
 
 lvim.builtin.which_key.mappings["t"] = {
-	name = "Diagnostics",
+  name = "Diagnostics",
 
-	t = { "<cmd>TroubleToggle<cr>", "Trouble" },
-	r = { "<cmd>TroubleToggle lsp_references<cr>", "References" },
-	f = { "<cmd>TroubleToggle lsp_definitions<cr>", "Definitions" },
-	d = { "<cmd>TroubleToggle lsp_document_diagnostics<cr>", "Document" },
-	q = { "<cmd>TroubleToggle quickfix<cr>", "Quick Fix" },
-	l = { "<cmd>TroubleToggle loclist<cr>", "Location List" },
-	w = { "<cmd>TroubleToggle lsp_workspace_diagnostics<cr>", "Workspace" },
+  t = { "<cmd>TroubleToggle<cr>", "Trouble" },
+  r = { "<cmd>TroubleToggle lsp_references<cr>", "References" },
+  f = { "<cmd>TroubleToggle lsp_definitions<cr>", "Definitions" },
+  d = { "<cmd>TroubleToggle lsp_document_diagnostics<cr>", "Document" },
+  q = { "<cmd>TroubleToggle quickfix<cr>", "Quick Fix" },
+  l = { "<cmd>TroubleToggle loclist<cr>", "Location List" },
+  w = { "<cmd>TroubleToggle lsp_workspace_diagnostics<cr>", "Workspace" },
 }
 
 -- User config for pre-defined plugins.
@@ -168,17 +169,17 @@ lvim.builtin.which_key.mappings["t"] = {
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
-	"bash",
-	"c",
-	"javascript",
-	"json",
-	"lua",
-	"python",
-	"typescript",
-	"css",
-	"rust",
-	"java",
-	"yaml",
+  "bash",
+  "c",
+  "javascript",
+  "json",
+  "lua",
+  "python",
+  "typescript",
+  "css",
+  "rust",
+  "java",
+  "yaml",
 }
 
 lvim.builtin.treesitter.ignore_install = { "haskell" }
@@ -222,254 +223,254 @@ lvim.builtin.treesitter.highlight.enabled = true
 
 -- Additional Plugins
 lvim.plugins = {
-	-- Dracula color scheme.
-	{ "dracula/vim",                  name = "dracula" },
+  -- Dracula color scheme.
+  { "Mofiqul/dracula.nvim",         name = "dracula" },
 
-	-- Move lines and selections.
-	{ "matze/vim-move",               event = "CursorMoved" },
+  -- Catppuccin color scheme.
+  { "catppuccin/nvim",              name = "catppuccin" },
 
-	-- Clojure
-	{ "Olical/conjure",               ft = { "clojure", "julia", "lua", "rust", "python" } },
+  -- Move lines and selections.
+  { "matze/vim-move",               event = "CursorMoved" },
 
-	-- Fast navigation. Alternative to Hop, Sneak and EasyMotion.
-	{ "ggandor/lightspeed.nvim",      event = "BufRead",                                   keys = { "s" } },
+  -- Clojure
+  { "Olical/conjure",               ft = { "clojure", "julia", "lua", "rust", "python" } },
 
-	-- Interactive live-evaluated scratchpad.
-	{ "metakirby5/codi.vim",          cmd = "Codi" },
+  -- Fast navigation. Alternative to Hop, Sneak and EasyMotion.
+  { "ggandor/lightspeed.nvim",      event = "BufRead",                                   keys = { "s" } },
 
-	-- Change surrounding pairs.
-	{ "tpope/vim-surround",           keys = { "c", "d", "y" } },
+  -- Interactive live-evaluated scratchpad.
+  { "metakirby5/codi.vim",          cmd = "Codi" },
 
-	-- Repeat mapped commands.
-	{ "tpope/vim-repeat",             keys = { "." } },
+  -- Change surrounding pairs.
+  { "tpope/vim-surround",           keys = { "c", "d", "y" } },
 
-	-- Diff View.
-	{ "sindrets/diffview.nvim",       cmd = { "DiffviewOpen" } },
+  -- Repeat mapped commands.
+  { "tpope/vim-repeat",             keys = { "." } },
 
-	-- Measures the startup time.
-	{ "tweekmonster/startuptime.vim", enabled = false },
-	-- {"dstein64/vim-startuptime", disable = true},
+  -- Diff View.
+  { "sindrets/diffview.nvim",       cmd = { "DiffviewOpen" } },
 
-	-- Trouble Diagnostics
-	{ "folke/trouble.nvim",           cmd = "TroubleToggle" },
+  -- Measures the startup time.
+  { "tweekmonster/startuptime.vim", enabled = false },
+  -- {"dstein64/vim-startuptime", disable = true},
 
-	-- Zen mode for writing.
-	{
-		"folke/zen-mode.nvim",
-		cmd = { "ZenMode" },
+  -- Trouble Diagnostics
+  { "folke/trouble.nvim",           cmd = "TroubleToggle" },
 
-		config = function()
-			require("zen-mode").setup({
-				window = {
-					backdrop = 0.95,
-					width = 0.60,
+  -- Zen mode for writing.
+  {
+    "folke/zen-mode.nvim",
+    cmd = { "ZenMode" },
 
-					options = {
-						signcolumn = "no", -- disable signcolumn
-						number = false,   -- disable number column
-						relativenumber = false, -- disable relative numbers
-						cursorline = false, -- disable cursorline
-						cursorcolumn = false, -- disable cursor column
-						foldcolumn = "0", -- disable fold column
-						list = false,     -- disable whitespace characters
-					},
-				},
+    config = function()
+      require("zen-mode").setup({
+        window = {
+          backdrop = 0.95,
+          width = 0.60,
 
-				plugins = {
-					kitty = { enabled = false },
-					gitsigns = { enabled = false },
-					tmux = { enabled = false },
-					twilight = { enabled = false },
-				},
-			})
-		end,
-	},
+          options = {
+            signcolumn = "no", -- disable signcolumn
+            number = false,   -- disable number column
+            relativenumber = false, -- disable relative numbers
+            cursorline = false, -- disable cursorline
+            cursorcolumn = false, -- disable cursor column
+            foldcolumn = "0", -- disable fold column
+            list = false,     -- disable whitespace characters
+          },
+        },
 
-	-- Prisma syntax highlighting (not tree-sitter)
-	{ "pantharshit00/vim-prisma",      ft = { "prisma" } },
+        plugins = {
+          kitty = { enabled = false },
+          gitsigns = { enabled = false },
+          tmux = { enabled = false },
+          twilight = { enabled = false },
+        },
+      })
+    end,
+  },
 
-	-- Matchup (navigate to matching text, e.g. if-elseif-else-end)
-	{
-		"andymass/vim-matchup",
-		event = "CursorMoved",
+  -- Prisma syntax highlighting (not tree-sitter)
+  { "pantharshit00/vim-prisma",      ft = { "prisma" } },
 
-		config = function()
-			vim.g.matchup_matchparen_offscreen = { method = "popup" }
-		end,
-	},
+  -- Matchup (navigate to matching text, e.g. if-elseif-else-end)
+  {
+    "andymass/vim-matchup",
+    event = "CursorMoved",
 
-	-- Minimap.
-	{
-		"wfxr/minimap.vim",
-		build = "cargo install --locked code-minimap",
-		cmd = { "Minimap", "MinimapClose", "MinimapToggle", "MinimapRefresh", "MinimapUpdateHighlight" },
+    config = function()
+      vim.g.matchup_matchparen_offscreen = { method = "popup" }
+    end,
+  },
 
-		config = function()
-			vim.cmd("let g:minimap_width = 10")
-			vim.cmd("let g:minimap_auto_start = 1")
-			vim.cmd("let g:minimap_auto_start_win_enter = 1")
-		end,
-	},
+  -- Minimap.
+  {
+    "wfxr/minimap.vim",
+    build = "cargo install --locked code-minimap",
+    cmd = { "Minimap", "MinimapClose", "MinimapToggle", "MinimapRefresh", "MinimapUpdateHighlight" },
 
-	-- Peek at Line
-	{
-		"nacro90/numb.nvim",
-		event = "BufRead",
+    config = function()
+      vim.cmd("let g:minimap_width = 10")
+      vim.cmd("let g:minimap_auto_start = 1")
+      vim.cmd("let g:minimap_auto_start_win_enter = 1")
+    end,
+  },
 
-		config = function()
-			require("numb").setup({
-				show_numbers = true, -- Enable 'number' for the window while peeking
-				show_cursorline = true, -- Enable 'cursorline' for the window while peeking
-			})
-		end,
-	},
+  -- Peek at Line
+  {
+    "nacro90/numb.nvim",
+    event = "BufRead",
 
-	-- Improved quick-fix window.
-	{
-		"kevinhwang91/nvim-bqf",
-		-- event = { "BufRead", "BufNew" },
-		ft = { "qf" },
+    config = function()
+      require("numb").setup({
+        show_numbers = true, -- Enable 'number' for the window while peeking
+        show_cursorline = true, -- Enable 'cursorline' for the window while peeking
+      })
+    end,
+  },
 
-		config = function()
-			require("bqf").setup({
-				auto_enable = true,
-				preview = {
-					win_height = 12,
-					win_vheight = 12,
-					delay_syntax = 80,
-					border_chars = { "┃", "┃", "━", "━", "┏", "┓", "┗", "┛", "█" },
-				},
-				func_map = {
-					vsplit = "",
-					ptogglemode = "z,",
-					stoggleup = "",
-				},
-				filter = {
-					fzf = {
-						action_for = { ["ctrl-s"] = "split" },
-						extra_opts = { "--bind", "ctrl-o:toggle-all", "--prompt", "> " },
-					},
-				},
-			})
-		end,
-	},
+  -- Improved quick-fix window.
+  {
+    "kevinhwang91/nvim-bqf",
+    -- event = { "BufRead", "BufNew" },
+    ft = { "qf" },
 
-	-- Ranger Integration
-	{
-		"kevinhwang91/rnvimr",
-		cmd = "RnvimrToggle",
+    config = function()
+      require("bqf").setup({
+        auto_enable = true,
+        preview = {
+          win_height = 12,
+          win_vheight = 12,
+          delay_syntax = 80,
+          border_chars = { "┃", "┃", "━", "━", "┏", "┓", "┗", "┛", "█" },
+        },
+        func_map = {
+          vsplit = "",
+          ptogglemode = "z,",
+          stoggleup = "",
+        },
+        filter = {
+          fzf = {
+            action_for = { ["ctrl-s"] = "split" },
+            extra_opts = { "--bind", "ctrl-o:toggle-all", "--prompt", "> " },
+          },
+        },
+      })
+    end,
+  },
 
-		config = function()
-			vim.g.rnvimr_draw_border = 1
-			vim.g.rnvimr_pick_enable = 1
-			vim.g.rnvimr_bw_enable = 1
-		end,
-	},
+  -- Ranger Integration
+  {
+    "kevinhwang91/rnvimr",
+    cmd = "RnvimrToggle",
 
-	-- Snap Finder
-	{
-		"camspiers/snap",
-		rocks = "fzy",
-		cmd = "Snap",
+    config = function()
+      vim.g.rnvimr_draw_border = 1
+      vim.g.rnvimr_pick_enable = 1
+      vim.g.rnvimr_bw_enable = 1
+    end,
+  },
 
-		config = function()
-			local snap = require("snap")
-			local layout = snap.get("layout").bottom
-			local file = snap.config.file:with({ consumer = "fzy", layout = layout })
-			local vimgrep = snap.config.vimgrep:with({ layout = layout })
+  -- Snap Finder
+  {
+    "camspiers/snap",
+    rocks = "fzy",
+    cmd = "Snap",
 
-			snap.register.command("find_files", file({ producer = "ripgrep.file" }))
-			snap.register.command("buffers", file({ producer = "vim.buffer" }))
-			snap.register.command("oldfiles", file({ producer = "vim.oldfile" }))
-			snap.register.command("live_grep", vimgrep({}))
-		end,
-	},
+    config = function()
+      local snap = require("snap")
+      local layout = snap.get("layout").bottom
+      local file = snap.config.file:with({ consumer = "fzy", layout = layout })
+      local vimgrep = snap.config.vimgrep:with({ layout = layout })
 
-	-- Git Blame
-	{
-		"f-person/git-blame.nvim",
-		cmd = { "GitBlameEnable", "GitBlameToggle" },
+      snap.register.command("find_files", file({ producer = "ripgrep.file" }))
+      snap.register.command("buffers", file({ producer = "vim.buffer" }))
+      snap.register.command("oldfiles", file({ producer = "vim.oldfile" }))
+      snap.register.command("live_grep", vimgrep({}))
+    end,
+  },
 
-		config = function()
-			vim.cmd("highlight default link gitblame SpecialComment")
-			vim.g.gitblame_enabled = 0
-		end,
-	},
+  -- Git Blame
+  {
+    "f-person/git-blame.nvim",
+    cmd = { "GitBlameEnable", "GitBlameToggle" },
 
-	-- Git Wrapper
-	{
-		"tpope/vim-fugitive",
-		ft = { "fugitive" },
+    config = function()
+      vim.cmd("highlight default link gitblame SpecialComment")
+      vim.g.gitblame_enabled = 0
+    end,
+  },
 
-		cmd = {
-			"G",
-			"Git",
-			"Gdiffsplit",
-			"Gread",
-			"Gwrite",
-			"Ggrep",
-			"GMove",
-			"GDelete",
-			"GBrowse",
-			"GRemove",
-			"GRename",
-			"Glgrep",
-			"Gedit",
-		},
-	},
+  -- Git Wrapper
+  {
+    "tpope/vim-fugitive",
+    ft = { "fugitive" },
 
-	-- Telescope Project Manager
-	{
-		"nvim-telescope/telescope-project.nvim",
-		event = "BufWinEnter",
+    cmd = {
+      "G",
+      "Git",
+      "Gdiffsplit",
+      "Gread",
+      "Gwrite",
+      "Ggrep",
+      "GMove",
+      "GDelete",
+      "GBrowse",
+      "GRemove",
+      "GRename",
+      "Glgrep",
+      "Gedit",
+    },
+  },
 
-		config = function()
-			require("telescope").load_extension("project")
-		end,
-	},
+  -- Telescope Project Manager
+  {
+    "nvim-telescope/telescope-project.nvim",
+    event = "BufWinEnter",
 
-	-- Colorizer
-	{
-		"norcalli/nvim-colorizer.lua",
-		event = "BufRead",
+    config = function()
+      require("telescope").load_extension("project")
+    end,
+  },
 
-		config = function()
-			require("colorizer").setup({ "*" }, {
-				RGB = true,  -- #RGB hex codes
-				RRGGBB = true, -- #RRGGBB hex codes
-				RRGGBBAA = true, -- #RRGGBBAA hex codes
-				rgb_fn = true, -- CSS rgb() and rgba() functions
-				hsl_fn = true, -- CSS hsl() and hsla() functions
-				css = true,  -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-				css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
-			})
-		end,
-	},
+  -- Colorizer
+  {
+    "norcalli/nvim-colorizer.lua",
+    event = "BufRead",
 
-	-- Go to Definition/Implementation Preview Window
-	{
-		"rmagatti/goto-preview",
+    config = function()
+      require("colorizer").setup({ "*" }, {
+        RGB = true,  -- #RGB hex codes
+        RRGGBB = true, -- #RRGGBB hex codes
+        RRGGBBAA = true, -- #RRGGBBAA hex codes
+        rgb_fn = true, -- CSS rgb() and rgba() functions
+        hsl_fn = true, -- CSS hsl() and hsla() functions
+        css = true,  -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+        css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+      })
+    end,
+  },
 
-		config = function()
-			require("goto-preview").setup({
-				width = 70,           -- Width of the floating window
-				height = 10,          -- Height of the floating window
-				default_mappings = false, -- Bind default mappings
-				debug = false,        -- Print debug information
-				opacity = nil,        -- 0-100 opacity level of the floating window where 100 is fully transparent.
-				post_open_hook = nil, -- A function taking two arguments, a buffer and a window to be ran as a hook.
-			})
+  -- Go to Definition/Implementation Preview Window
+  {
+    "rmagatti/goto-preview",
 
-			vim.cmd("nnoremap gP <cmd>lua require('goto-preview').close_all_win()<CR>")
-			vim.cmd("nnoremap gpd <cmd>lua require('goto-preview').goto_preview_definition()<CR>")
-			vim.cmd("nnoremap gpi <cmd>lua require('goto-preview').goto_preview_implementation()<CR>")
-			vim.cmd("nnoremap gpr <cmd>lua require('goto-preview').goto_preview_references()<CR>")
-		end,
-	},
+    config = function()
+      require("goto-preview").setup({
+        width = 70,           -- Width of the floating window
+        height = 10,          -- Height of the floating window
+        default_mappings = false, -- Bind default mappings
+        debug = false,        -- Print debug information
+        opacity = nil,        -- 0-100 opacity level of the floating window where 100 is fully transparent.
+        post_open_hook = nil, -- A function taking two arguments, a buffer and a window to be ran as a hook.
+      })
 
-	-- Symbols Outline
-	{ "simrat39/symbols-outline.nvim", cmd = "SymbolsOutline" },
+      vim.cmd("nnoremap gP <cmd>lua require('goto-preview').close_all_win()<CR>")
+      vim.cmd("nnoremap gpd <cmd>lua require('goto-preview').goto_preview_definition()<CR>")
+      vim.cmd("nnoremap gpi <cmd>lua require('goto-preview').goto_preview_implementation()<CR>")
+      vim.cmd("nnoremap gpr <cmd>lua require('goto-preview').goto_preview_references()<CR>")
+    end,
+  },
 
-	-- I'm out of jobs now
-	{ "github/copilot" }
+  -- Symbols Outline
+  { "simrat39/symbols-outline.nvim", cmd = "SymbolsOutline" },
 }
