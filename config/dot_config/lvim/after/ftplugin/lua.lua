24f3f4205fc6a050d42014d79fb43f9aabc903fc
Phoomparin Mano <phoomparin@gmail.com>
2023-06-02 15:10:50 +0700

    chore: lunarvim

--- config/dot_config/lvim/after/ftplugin/lua.lua
+++
@@ -1,5 +0,0 @@
-local formatters = require("lvim.lsp.null-ls.formatters")
-
-formatters.setup({
-	{ exe = "stylua", filetypes = { "lua" } },
-})
--- config/dot_config/lvim/after/ftplugin/python.lua
+++
@@ -1,6 +0,0 @@
-local formatters = require("lvim.lsp.null-ls.formatters")
-
-formatters.setup({
-	{ exe = "black", filetypes = { "python" } },
-	{ exe = "isort", filetypes = { "python" } },
-})
