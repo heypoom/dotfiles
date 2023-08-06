ae92aad50e6f2b48723dc098980fce2c59308d15
Phoomparin Mano <phoomparin@gmail.com>
2023-08-06 12:16:17 +0700

    chore: update emacs notes dir

--- config/dot_doom.d/config.el
+++ config/dot_doom.d/config.el
@@ -28,8 +28,8 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/Notes")
-(setq org-roam-directory "~/Notes/Garden")
+(setq org-directory "~/notes")
+(setq org-roam-directory "~/notes")
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
