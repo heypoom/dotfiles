6516c0f42f072ea89ecb9fa642936862139ee9f9
Phoomparin Mano <phoomparin@gmail.com>
2023-08-06 12:21:05 +0700

    chore: store in chezmoi dir instead

---
+++ raycast/README.md
@@ -0,0 +1,5 @@
+# Raycast Script Commands and Extensions
+
+- Script commands: https://github.com/raycast/script-commands
+- Extensions: https://developers.raycast.com/basics/create-your-first-extension
+
---
+++ raycast/scripts/publish-digital-garden.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Publish digital garden
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🤖
+# @raycast.packageName Poom
+
+# Documentation:
+# @raycast.description Publish the Obsidian vault to my digital garden's MongoDB database
+# @raycast.author heypoom
+# @raycast.authorURL https://raycast.com/heypoom
+
+echo "Hello World!"
+
