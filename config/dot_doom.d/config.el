3f33aa3655f476ff70d457495da50af4088ac21e
Phoomparin Mano <phoomparin@gmail.com>
2023-08-14 15:06:20 +0700

    chore: doom server

--- config/dot_doom.d/config.el
+++ config/dot_doom.d/config.el
@@ -3,8 +3,6 @@
 ;; Place your private configuration here! Remember, you do not need to run 'doom
 ;; sync' after modifying this file!
 
-(add-to-list 'default-frame-alist '(undecorated-round . t))
-
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
 (setq user-full-name "Phoomparin Mano"
--- config/dot_doom.d/init.el
+++ config/dot_doom.d/init.el
@@ -14,6 +14,9 @@
 ;;      Alternatively, press 'gd' (or 'C-c c d') on a module to browse its
 ;;      directory (for easy access to its source code).
 
+(setq server-socket-dir "~/.emacs.d/server")
+(server-start)
+
 (doom! :input
        ;;chinese
        ;;japanese
