17efc9a83d8f99a88be41e9d4afbc8193f962f6a
Phoomparin Mano <phoomparin@gmail.com>
2023-08-15 20:23:43 +0700

    chore: change sketchybar rc

--- config/dot_config/sketchybar/plugins/executable_front_app.sh
+++ config/dot_config/sketchybar/plugins/executable_front_app.sh
@@ -1,8 +1,68 @@
-#!/bin/sh
+#!/usr/bin/env zsh
 
-# Some events send additional information specific to the event in the $INFO
-# variable. E.g. the front_app_switched event sends the name of the newly
-# focused application in the $INFO variable:
-# https://felixkratz.github.io/SketchyBar/config/events#events-and-scripting
+ICON_PADDING_RIGHT=5
 
-sketchybar --set $NAME label="$INFO"
+case $INFO in
+"Arc")
+    ICON_PADDING_RIGHT=5
+    ICON=󰞍
+    ;;
+"Code")
+    ICON_PADDING_RIGHT=4
+    ICON=󰨞
+    ;;
+"Calendar")
+    ICON_PADDING_RIGHT=3
+    ICON=
+    ;;
+"Discord")
+    ICON=󰙯
+    ;;
+"FaceTime")
+    ICON_PADDING_RIGHT=5
+    ICON=
+    ;;
+"Finder")
+    ICON=
+    ;;
+"Google Chrome")
+    ICON_PADDING_RIGHT=7
+    ICON=
+    ;;
+"IINA")
+    ICON_PADDING_RIGHT=4
+    ICON=󰕼
+    ;;
+"kitty")
+    ICON=󰄛
+    ;;
+"Messages")
+    ICON=󰍦
+    ;;
+"Notion")
+    ICON_PADDING_RIGHT=6
+    ICON=󰈄
+    ;;
+"Preview")
+    ICON_PADDING_RIGHT=3
+    ICON=
+    ;;
+"PS Remote Play")
+    ICON_PADDING_RIGHT=3
+    ICON=
+    ;;
+"TextEdit")
+    ICON_PADDING_RIGHT=4
+    ICON=
+    ;;
+"Transmission")
+    ICON_PADDING_RIGHT=3
+    ICON=󰶘
+    ;;
+*)
+    ICON=﯂
+    ;;
+esac
+
+sketchybar --set $NAME icon=$ICON icon.padding_right=$ICON_PADDING_RIGHT
+sketchybar --set $NAME.name label="$INFO"
--- config/dot_config/sketchybar/plugins/executable_volume.sh
+++ config/dot_config/sketchybar/plugins/executable_volume.sh
@@ -1,56 +1,18 @@
-#!/bin/bash
+#!/usr/bin/env zsh
 
-WIDTH=100
-
-volume_change() {
-  source "$CONFIG_DIR/icons.sh"
-  case $INFO in
-    [6-9][0-9]|100) ICON=$VOLUME_100
+case ${INFO} in
+0)
+    ICON=""
+    ICON_PADDING_RIGHT=21
     ;;
-    [3-5][0-9]) ICON=$VOLUME_66
+[0-9])
+    ICON=""
+    ICON_PADDING_RIGHT=12
     ;;
-    [1-2][0-9]) ICON=$VOLUME_33
+*)
+    ICON=""
+    ICON_PADDING_RIGHT=6
     ;;
-    [1-9]) ICON=$VOLUME_10
-    ;;
-    0) ICON=$VOLUME_0
-    ;;
-    *) ICON=$VOLUME_100
-  esac
-
-  sketchybar --set volume_icon label=$ICON
-
-  sketchybar --set $NAME slider.percentage=$INFO \
-             --animate tanh 30 --set $NAME slider.width=$WIDTH 
-
-  sleep 2
-
-  # Check wether the volume was changed another time while sleeping
-  FINAL_PERCENTAGE=$(sketchybar --query $NAME | jq -r ".slider.percentage")
-  if [ "$FINAL_PERCENTAGE" -eq "$INFO" ]; then
-    sketchybar --animate tanh 30 --set $NAME slider.width=0
-  fi
-}
-
-mouse_clicked() {
-  osascript -e "set volume output volume $PERCENTAGE"
-}
-
-mouse_entered() {
-  sketchybar --set $NAME slider.knob.drawing=on
-}
-
-mouse_exited() {
-  sketchybar --set $NAME slider.knob.drawing=off
-}
-
-case "$SENDER" in
-  "volume_change") volume_change
-  ;;
-  "mouse.clicked") mouse_clicked
-  ;;
-  "mouse.entered") mouse_entered
-  ;;
-  "mouse.exited") mouse_exited
-  ;;
 esac
+
+sketchybar --set $NAME icon=$ICON icon.padding_right=$ICON_PADDING_RIGHT label="$INFO%"
