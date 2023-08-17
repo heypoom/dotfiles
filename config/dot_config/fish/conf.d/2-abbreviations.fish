c656be6886c01121e5c7bdd923b1cd641cbabc20
Phoomparin Mano <phoomparin@gmail.com>
2023-08-17 13:13:43 +0700

    chore: abbr

--- config/dot_config/fish/conf.d/2-abbreviations.fish
+++ config/dot_config/fish/conf.d/2-abbreviations.fish
@@ -4,6 +4,15 @@ abbr -a vim lvim
 abbr -a gaa 'git add --all'
 abbr -a caski 'brew install --cask'
 abbr -a g git
+abbr -a gfo 'git fetch origin'
+abbr -a gl 'git pull'
+abbr -a gp 'git push'
+abbr -a gc 'git commit'
+abbr -a gco 'git checkout'
+abbr -a gd 'git diff'
+abbr -a gdh 'git diff HEAD'
+abbr -a gst 'git stash'
+abbr -a gstp 'git stash pop'
 abbr -a scripts 'jq .scripts package.json'
 abbr -a staging 'git fetch origin staging && git merge origin/staging'
 abbr -a vi lvim
@@ -11,21 +20,14 @@ abbr -a lv lvim
 abbr -a nv nvim
 abbr -a nvd 'nvim +Nvdash'
 abbr -a ls exa
-abbr -a gl 'git pull'
 abbr -a ch chezmoi
 abbr -a chez chezmoi
 abbr -a tree 'exa --tree'
 abbr -a grv 'gh repo view -w'
 abbr -a ta 'tmux attach'
 abbr -a l exa
-abbr -a gstp 'git stash pop'
-abbr -a gp 'git push'
-abbr -a gc 'git commit'
 abbr -a deps 'jq .dependencies package.json'
 abbr -a v lvim
-abbr -a gco 'git checkout'
-abbr -a gd 'git diff'
-abbr -a gdh 'git diff HEAD'
 abbr -a tns 'tmux new -s'
 abbr -a devdeps 'jq .devDependencies package.json'
 abbr -a o chatgpt
