30acea847ad57a6a5cd3a52490072a6cc4168215
Phoomparin Mano <phoomparin@gmail.com>
2023-12-23 17:38:09 +0700

    feat: add flash-nvim

--- config/dot_config/nvchad/lua/custom/plugins.lua
+++ config/dot_config/nvchad/lua/custom/plugins.lua
@@ -45,6 +45,54 @@ local plugins = {
         "stylua"
       },
     },
+  },
+
+  {
+    "tpope/vim-repeat",
+    keys = { "." },
+  },
+
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    opts = {},
+
+    keys = {
+      {
+        "s",
+        mode = { "n", "x", "o" },
+        function() require("flash").jump() end,
+        desc = "Flash" 
+      },
+
+      {
+        "S",
+        mode = { "n", "x", "o" },
+        function() require("flash").treesitter() end,
+        desc = "Flash Treesitter"
+      },
+
+      {
+        "r",
+        mode = "o",
+        function() require("flash").remote() end,
+        desc = "Remote Flash"
+      },
+
+      {
+        "R",
+        mode = { "o", "x" },
+        function() require("flash").treesitter_search() end,
+        desc = "Treesitter Search"
+      },
+
+      {
+        "<c-s>",
+        mode = { "c" },
+        function() require("flash").toggle() end,
+        desc = "Toggle Flash Search"
+      },
+    },
   }
 }
 
