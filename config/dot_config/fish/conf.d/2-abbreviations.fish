c7576eaf52174b93f489f91b9e4193e53f037ccc
Phoomparin Mano <phoomparin@gmail.com>
2023-12-23 16:17:07 +0700

    feat: neovim distro abbrevs

--- config/dot_config/fish/conf.d/2-abbreviations.fish
+++ config/dot_config/fish/conf.d/2-abbreviations.fish
@@ -20,11 +20,13 @@ abbr -a lv lvim
 abbr -a vich 'chezmoi edit --apply'
 abbr -a nv 'env NVIM_APPNAME=nvchad nvim'
 abbr -a nvchad 'env NVIM_APPNAME=nvchad nvim'
+abbr -a nvim-base 'nvim'
 abbr -a nvim-chad 'env NVIM_APPNAME=nvchad nvim'
 abbr -a lazyvim 'env NVIM_APPNAME=lazyvim nvim'
 abbr -a nvim-lazy 'env NVIM_APPNAME=lazyvim nvim'
 abbr -a nvim-astro 'env NVIM_APPNAME=astro nvim'
 abbr -a nvim-kick 'env NVIM_APPNAME=kickstart nvim'
+abbr -a nvim-lunar 'lvim'
 abbr -a ls exa
 abbr -a ch chezmoi
 abbr -a chez chezmoi
