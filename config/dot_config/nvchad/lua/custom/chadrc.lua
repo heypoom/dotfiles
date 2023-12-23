db1e145180103061e5d3be25e407a78c175284fb
Phoomparin Mano <phoomparin@gmail.com>
2023-12-23 16:59:39 +0700

    feat: add default languages to nvchad

--- config/dot_config/nvchad/lua/custom/chadrc.lua
+++ config/dot_config/nvchad/lua/custom/chadrc.lua
@@ -2,5 +2,6 @@
 local M = {}
 
 M.ui = { theme = 'catppuccin' }
+M.plugins = "custom.plugins"
 
 return M
---
+++ config/dot_config/nvchad/lua/custom/plugins.lua
@@ -0,0 +1,23 @@
+local plugins = {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "html",
+        "css",
+        "javascript",
+        "typescript",
+        "tsx",
+        "json",
+        "vue",
+        "svelte",
+        "c",
+        "rust"
+      },
+    },
+  },
+}
+
+return plugins
