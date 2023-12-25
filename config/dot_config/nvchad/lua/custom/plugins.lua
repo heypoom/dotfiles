e9d01d3d08f3134b69a53233b677a30360306d74
Phoomparin Mano <phoomparin@gmail.com>
2023-12-26 01:12:37 +0700

    feat: otter and golang

--- config/dot_config/nvchad/lua/custom/plugins.lua
+++ config/dot_config/nvchad/lua/custom/plugins.lua
@@ -274,6 +274,28 @@ local plugins = {
     event = "InsertEnter",
     opts = {}
   },
+
+  -- Otter
+  {
+    'jmbuhr/otter.nvim',
+    event = "VeryLazy",
+  },
+
+  -- Golang
+  {
+    "ray-x/go.nvim",
+    dependencies = {
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("go").setup()
+    end,
+    event = {"CmdlineEnter"},
+    ft = {"go", 'gomod'},
+    build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+  },
 }
 
 return plugins
