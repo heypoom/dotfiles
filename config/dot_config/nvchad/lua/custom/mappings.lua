91a22fec6972b65299f60739b799d929afac7a39
Phoomparin Mano <phoomparin@gmail.com>
2023-12-26 03:06:37 +0700

    feat: add arrow up down for completions

--- config/dot_config/nvchad/lua/custom/mappings.lua
+++ config/dot_config/nvchad/lua/custom/mappings.lua
@@ -1,3 +1,29 @@
 local M = {}
 
+M.cmp = {
+  i = {
+    ["<Up>"] = {
+      function()
+        local cmp = require("cmp")
+
+        if cmp.visible() then
+          cmp.select_prev_item()
+        end
+      end,
+      "select previous item",
+    },
+
+    ["<Down>"] = {
+      function()
+        local cmp = require("cmp")
+
+        if cmp.visible() then
+          cmp.select_next_item()
+        end
+      end,
+      "select next item",
+    },
+  }
+}
+
 return M
