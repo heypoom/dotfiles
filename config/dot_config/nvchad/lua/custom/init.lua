362ca14f37b4edfcc6d78e04a992bc35b09034f9
Phoomparin Mano <phoomparin@gmail.com>
2023-12-27 15:38:50 +0700

    feat: add neovide config

--- config/dot_config/nvchad/lua/custom/init.lua
+++ config/dot_config/nvchad/lua/custom/init.lua
@@ -19,31 +19,4 @@ vim.g.vscode_snippets_path = vim.fn.expand("$HOME/.local/share/chezmoi/config/vs
 vim.g.snipmate_snippets_path = vim.fn.stdpath "config" .. "/lua/custom/snippets"
 vim.g.lua_snippets_path = vim.fn.stdpath "config" .. "/lua/custom/lua-snippets"
 
--- Neovide
-if vim.g.neovide then
-  vim.o.guifont = "Zed Mono Extended:h18"
-  vim.opt.linespace = 0
-  vim.g.neovide_scale_factor = 1.0
-  vim.g.neovide_padding_top = 0
-  vim.g.neovide_padding_bottom = 0
-  vim.g.neovide_padding_right = 0
-  vim.g.neovide_padding_left = 0
-  vim.g.neovide_floating_blur_amount_x = 2.0
-  vim.g.neovide_floating_blur_amount_y = 2.0
-  vim.g.neovide_scroll_animation_length = 0.3
-  vim.g.neovide_cursor_vfx_mode = ""
-  vim.g.neovide_cursor_vfx_opacity = 200.0
-  vim.g.neovide_hide_mouse_when_typing = false
-  vim.g.neovide_theme = 'auto'
-  vim.g.neovide_unlink_border_highlights = true
-
-  local alpha = function()
-    return string.format("%x", math.floor(255 * (vim.g.transparency or 0.8)))
-  end
-
-  -- g:neovide_transparency should be 0 if you want to unify transparency of content and title bar.
-  vim.g.neovide_transparency = 0.0
-  vim.g.transparency = 0.8
-  vim.g.neovide_background_color = "#0f1117" .. alpha()
-end
-
+require "neovide"
