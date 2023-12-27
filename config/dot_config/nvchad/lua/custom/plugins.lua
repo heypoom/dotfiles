5be45046af7a3646a0fef0ca3458284fbb77fc30
Phoomparin Mano <phoomparin@gmail.com>
2023-12-27 15:22:35 +0700

    feat: add file browser

--- config/dot_config/nvchad/lua/custom/plugins.lua
+++ config/dot_config/nvchad/lua/custom/plugins.lua
@@ -258,11 +258,21 @@ local plugins = {
   -- Telescope Project integration
   {
     "nvim-telescope/telescope-project.nvim",
+    cmd = "Telescope",
+
+    dependencies = {
+      "nvim-telescope/telescope-file-browser.nvim",
+      dependencies = {
+        "nvim-telescope/telescope.nvim",
+        "nvim-lua/plenary.nvim"
+      }
+    },
 
     config = function()
       local telescope = require("telescope")
 
       telescope.load_extension("project")
+      telescope.load_extension("file_browser")
     end,
   },
 
