469dd2879390f452fda4a50cb2e6498ebc3182cf
Phoomparin Mano <phoomparin@gmail.com>
2023-12-27 13:34:18 +0700

    feat: add multi cursor

--- config/dot_config/nvchad/lua/custom/plugins.lua
+++ config/dot_config/nvchad/lua/custom/plugins.lua
@@ -400,6 +400,25 @@ local plugins = {
     branch = "harpoon2",
     requires = {"nvim-lua/plenary.nvim"},
   },
+
+  -- Multi Cursors
+  {
+    "smoka7/multicursors.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      'smoka7/hydra.nvim',
+    },
+    opts = {},
+    cmd = { 'MCstart', 'MCvisual', 'MCclear', 'MCpattern', 'MCvisualPattern', 'MCunderCursor' },
+    keys = {
+      {
+        mode = { 'v', 'n' },
+        '<Leader>m',
+        '<cmd>MCstart<cr>',
+        desc = 'Create a selection for selected text or word under the cursor',
+      },
+    },
+  }
 }
 
 return plugins
