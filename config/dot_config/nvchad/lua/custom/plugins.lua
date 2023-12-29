4e3480bb5f62b53356d4ce144f5f6f620fa9fc6e
Phoomparin Mano <phoomparin@gmail.com>
2023-12-29 16:53:05 +0700

    feat: markdown preview

--- config/dot_config/nvchad/lua/custom/plugins.lua
+++ config/dot_config/nvchad/lua/custom/plugins.lua
@@ -633,6 +633,16 @@ local plugins = {
       },
     },
   },
+
+  -- Markdown Preview
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+  },
 }
 
 return plugins
