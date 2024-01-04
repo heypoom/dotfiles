43120b89b25e1b7680a2a212c953faa1dd05b5b6
Phoomparin Mano <phoomparin@gmail.com>
2024-01-04 17:34:25 +0700

    feat: add emacs kill ring

--- config/dot_config/nvchad/lua/custom/plugins.lua
+++ config/dot_config/nvchad/lua/custom/plugins.lua
@@ -285,6 +285,7 @@ local plugins = {
 
     dependencies = {
       "nvim-telescope/telescope-file-browser.nvim",
+
       dependencies = {
         "nvim-telescope/telescope.nvim",
         "nvim-lua/plenary.nvim",
@@ -750,6 +751,50 @@ local plugins = {
     cmd = { "Spectre" },
     config = function()
       require("spectre").setup {}
+    end,
+  },
+
+  -- Emacs-like Kill Ring
+  {
+    "gbprod/yanky.nvim",
+
+    config = function()
+      require("yanky").setup {
+        ring = {
+          history_length = 100,
+          storage = "shada",
+          storage_path = vim.fn.stdpath "data" .. "/databases/yanky.db", -- Only for sqlite storage
+          sync_with_numbered_registers = true,
+          cancel_event = "update",
+          ignore_registers = { "_" },
+          update_register_on_cycle = false,
+        },
+        picker = {
+          select = {
+            action = nil, -- nil to use default put action
+          },
+          telescope = {
+            use_default_mappings = true, -- if default mappings should be used
+            mappings = nil, -- nil to use default mappings or no mappings (see `use_default_mappings`)
+          },
+        },
+        system_clipboard = {
+          sync_with_ring = true,
+        },
+        highlight = {
+          on_put = true,
+          on_yank = true,
+          timer = 200,
+        },
+        preserve_cursor_position = {
+          enabled = true,
+        },
+        textobj = {
+          enabled = true,
+        },
+      }
+
+      require("telescope").load_extension "yank_history"
     end,
   },
 }
