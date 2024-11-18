612cf3f07045630edeccd2f432d05334698b9bb0
Phoomparin Mano <phoomparin@gmail.com>
2024-11-18 22:05:41 +0700

    disable vscode plugins for now

--- config/dot_config/vscode-nvim/init.lua
+++ config/dot_config/vscode-nvim/init.lua
@@ -19,102 +19,102 @@ vim.g.mapleader = " "
 
 -- setup lazy
 require("lazy").setup({
-	-- Matchup
-	{
-		"andymass/vim-matchup",
-		lazy = false,
-		setup = function()
-			vim.g.matchup_matchparen_offscreen = { method = "popup" }
-		end,
-	},
-
-	-- WhichKey
-	"folke/which-key.nvim",
-
-	-- NeoConf
-	{ "folke/neoconf.nvim", cmd = "Neoconf" },
-
-	-- NeoDev
-	"folke/neodev.nvim",
-
-	-- Flash
-	{
-		"folke/flash.nvim",
-		event = "VeryLazy",
-		opts = {},
-		keys = {
-			{
-				"s",
-				mode = { "n", "x", "o" },
-				function()
-					require("flash").jump()
-				end,
-				desc = "Flash",
-			},
-			{
-				"S",
-				mode = { "n", "x", "o" },
-				function()
-					require("flash").treesitter()
-				end,
-				desc = "Flash Treesitter",
-			},
-			{
-				"r",
-				mode = "o",
-				function()
-					require("flash").remote()
-				end,
-				desc = "Remote Flash",
-			},
-			{
-				"R",
-				mode = { "o", "x" },
-				function()
-					require("flash").treesitter_search()
-				end,
-				desc = "Treesitter Search",
-			},
-			{
-				"<c-s>",
-				mode = { "c" },
-				function()
-					require("flash").toggle()
-				end,
-				desc = "Toggle Flash Search",
-			},
-		},
-	},
-
-	-- Multi Cursor
-	{
-		"vscode-neovim/vscode-multi-cursor.nvim",
-		event = "VeryLazy",
-		cond = not not vim.g.vscode,
-		config = function()
-			-- setup vscode multi cursor
-			local C = require("vscode-multi-cursor")
-
-			C.setup({
-				default_mappings = true,
-				no_selection = false,
-			})
-
-			vim.keymap.set({ "n", "x", "i" }, "<C-d>", function()
-				C.addSelectionToNextFindMatch()
-			end)
-		end,
-	},
-
-	-- Surround
-	{
-		"kylechui/nvim-surround",
-		version = "*", -- Use for stability; omit to use `main` branch for the latest features
-		event = "VeryLazy",
-		config = function()
-			require("nvim-surround").setup({})
-		end,
-	},
+	-- -- Matchup
+	-- {
+	-- 	"andymass/vim-matchup",
+	-- 	lazy = false,
+	-- 	setup = function()
+	-- 		vim.g.matchup_matchparen_offscreen = { method = "popup" }
+	-- 	end,
+	-- },
+	--
+	-- -- WhichKey
+	-- "folke/which-key.nvim",
+	--
+	-- -- NeoConf
+	-- { "folke/neoconf.nvim", cmd = "Neoconf" },
+	--
+	-- -- NeoDev
+	-- "folke/neodev.nvim",
+	--
+	-- -- Flash
+	-- {
+	-- 	"folke/flash.nvim",
+	-- 	event = "VeryLazy",
+	-- 	opts = {},
+	-- 	keys = {
+	-- 		{
+	-- 			"s",
+	-- 			mode = { "n", "x", "o" },
+	-- 			function()
+	-- 				require("flash").jump()
+	-- 			end,
+	-- 			desc = "Flash",
+	-- 		},
+	-- 		{
+	-- 			"S",
+	-- 			mode = { "n", "x", "o" },
+	-- 			function()
+	-- 				require("flash").treesitter()
+	-- 			end,
+	-- 			desc = "Flash Treesitter",
+	-- 		},
+	-- 		{
+	-- 			"r",
+	-- 			mode = "o",
+	-- 			function()
+	-- 				require("flash").remote()
+	-- 			end,
+	-- 			desc = "Remote Flash",
+	-- 		},
+	-- 		{
+	-- 			"R",
+	-- 			mode = { "o", "x" },
+	-- 			function()
+	-- 				require("flash").treesitter_search()
+	-- 			end,
+	-- 			desc = "Treesitter Search",
+	-- 		},
+	-- 		{
+	-- 			"<c-s>",
+	-- 			mode = { "c" },
+	-- 			function()
+	-- 				require("flash").toggle()
+	-- 			end,
+	-- 			desc = "Toggle Flash Search",
+	-- 		},
+	-- 	},
+	-- },
+	--
+	-- -- Multi Cursor
+	-- {
+	-- 	"vscode-neovim/vscode-multi-cursor.nvim",
+	-- 	event = "VeryLazy",
+	-- 	cond = not not vim.g.vscode,
+	-- 	config = function()
+	-- 		-- setup vscode multi cursor
+	-- 		local C = require("vscode-multi-cursor")
+	--
+	-- 		C.setup({
+	-- 			default_mappings = true,
+	-- 			no_selection = false,
+	-- 		})
+	--
+	-- 		vim.keymap.set({ "n", "x", "i" }, "<C-d>", function()
+	-- 			C.addSelectionToNextFindMatch()
+	-- 		end)
+	-- 	end,
+	-- },
+	--
+	-- -- Surround
+	-- {
+	-- 	"kylechui/nvim-surround",
+	-- 	version = "*", -- Use for stability; omit to use `main` branch for the latest features
+	-- 	event = "VeryLazy",
+	-- 	config = function()
+	-- 		require("nvim-surround").setup({})
+	-- 	end,
+	-- },
 })
 
 -- setup unnamed clipboard
