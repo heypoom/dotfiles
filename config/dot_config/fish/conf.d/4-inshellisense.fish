63f5516087f4f12cfaf32398744a5ad65c3efca8
Phoomparin Mano <phoomparin@gmail.com>
2024-12-04 15:00:02 +0700

    remove inshellisense

--- config/dot_config/fish/conf.d/4-inshellisense.fish
+++
@@ -1,9 +0,0 @@
-# ---------------- inshellisense shell plugin ----------------
-if test -z "$ISTERM" && status --is-interactive && type -q is
-  if status --is-login
-    is -s fish --login ; kill %self
-  else
-    is -s fish ; kill %self
-  end
-end
-
