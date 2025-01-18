f8c1f03f9ccbab16a7350ba75d87c5ea91503945
Phoomparin Mano <phoomparin@gmail.com>
2025-01-18 10:16:07 +0700

    update fish abbrs

--- config/dot_config/fish/conf.d/2-abbreviations.fish
+++ config/dot_config/fish/conf.d/2-abbreviations.fish
@@ -1,31 +1,17 @@
+abbr -a zz "zellij"
+abbr -a zj zellij
+abbr -a zjd 'zellij --layout default'
+
 abbr -a dev "yarn dev"
 abbr -a scr "scripts"
 abbr -a sb "yarn storybook"
 
 abbr -a raya "env TERM=xterm-256color ssh raya"
-
-abbr -a brewi 'brew install'
-abbr -a gpv 'gh pr view -w'
-abbr -a vim lvim
-abbr -a gaa 'git add --all'
-abbr -a caski 'brew install --cask'
-abbr -a g git
-abbr -a gfo 'git fetch origin'
-abbr -a gl 'git pull'
-abbr -a gp 'git push'
-abbr -a gc 'git commit'
-abbr -a gco 'git checkout'
-abbr -a gd 'git diff'
-abbr -a gdh 'git diff HEAD'
-abbr -a gst 'git stash'
-abbr -a gstp 'git stash pop'
-abbr -a scripts 'jq .scripts package.json'
-abbr -a staging 'git fetch origin staging && git merge origin/staging'
+abbr -a edit-abbr "chezmoi edit --apply ~/.config/fish/conf.d/2-abbreviations.fish"
 
 abbr -a v nvim
 abbr -a vi nvim
 abbr -a lv nvim
-
 abbr -a vich 'chezmoi edit --apply'
 abbr -a nv 'env NVIM_APPNAME=nvchad nvim'
 abbr -a nvchad 'env NVIM_APPNAME=nvchad nvim'
@@ -49,8 +35,24 @@ abbr -a deps 'jq .dependencies package.json'
 abbr -a tns 'tmux new -s'
 abbr -a devdeps 'jq .devDependencies package.json'
 abbr -a o chatgpt
-abbr -a zj zellij
-abbr -a zjd 'zellij --layout default'
+
+abbr -a brewi 'brew install'
+abbr -a gpv 'gh pr view -w'
+abbr -a vim lvim
+abbr -a gaa 'git add --all'
+abbr -a caski 'brew install --cask'
+abbr -a g git
+abbr -a gfo 'git fetch origin'
+abbr -a gl 'git pull'
+abbr -a gp 'git push'
+abbr -a gc 'git commit'
+abbr -a gco 'git checkout'
+abbr -a gd 'git diff'
+abbr -a gdh 'git diff HEAD'
+abbr -a gst 'git stash'
+abbr -a gstp 'git stash pop'
+abbr -a scripts 'jq .scripts package.json'
+abbr -a staging 'git fetch origin staging && git merge origin/staging'
 
 # Neovim Codi abbreviations (live playground)
 abbr -a crb 'lvim "+Codi ruby"'
