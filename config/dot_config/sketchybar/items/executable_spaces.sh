7363320ca2119dee02f6e2b2eb3da387da8090ab
Phoomparin Mano <phoomparin@gmail.com>
2025-01-18 11:07:39 +0700

    update sketchybar

--- config/dot_config/sketchybar/items/executable_spaces.sh
+++ config/dot_config/sketchybar/items/executable_spaces.sh
@@ -4,27 +4,4 @@ source "$HOME/.config/sketchybar/config.sh"
 
 count=$(yabai -m query --spaces | jq length)
 
-for ((i = 1; i <= 10; i++))
-do
-	space="space.$i"
 
-	sketchybar --add space $space left \
-	           --set $space associated_space=$i \
-							icon="$i" \
-							icon.padding_left=10 \
-							icon.padding_right=5 \
-							icon.color=$WHITE \
-							icon.highlight_color=$BG \
-							icon.font="$FONT_FACE:Bold:14.0" \
-							script="$PLUGIN_SHARED_DIR/space.sh" \
-	           --subscribe $space space_change mouse.clicked
-done
-
-sketchybar --add item add_space left \
-    --set add_space \
-    background.color=0xee232639 \
-    icon.padding_left=10 \
-    icon.padding_right=5 \
-		icon.font="$FONT_FACE:Bold:14.0" \
-    script="$PLUGIN_SHARED_DIR/add_space.sh" \
-    --subscribe add_space space_change mouse.clicked
