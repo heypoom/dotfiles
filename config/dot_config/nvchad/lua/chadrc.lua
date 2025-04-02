c6cb0575a371d5a68a554546c9d5f321414c4d03
Phoomparin Mano <phoomparin@gmail.com>
2025-04-03 02:11:35 +0700

    nvchad transparent

--- config/dot_config/nvchad/lua/chadrc.lua
+++ config/dot_config/nvchad/lua/chadrc.lua
@@ -1,18 +1,41 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
--- Please read that file to know all available options :( 
+-- Please read that file to know all available options :(
 
 ---@type ChadrcConfig
 local M = {}
 
-M.base46 = {
-	theme = "onedark",
+M.ui = {
+	theme = "catppuccin",
+	transparency = true,
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+	statusline = {
+		theme = "vscode",
+		separator_style = "default",
+		overriden_modules = nil,
+	},
+
+	tabufline = {
+		lazyload = true,
+		overriden_modules = function(modules)
+			table.remove(modules, 4)
+		end,
+	},
+
+	-- refer to [nvchad.com/docs/config/theming]
+	hl_override = {},
+	changed_themes = {},
+	extended_integrations = { "trouble", "alpha", "dap" },
 }
+
+-- M.base46 = {
+-- 	theme = "onedark",
+
+-- 	-- hl_override = {
+-- 	-- 	Comment = { italic = true },
+-- 	-- 	["@comment"] = { italic = true },
+-- 	-- },
+-- }
 
 -- M.nvdash = { load_on_startup = true }
 -- M.ui = {
