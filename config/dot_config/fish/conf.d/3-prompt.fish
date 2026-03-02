be1dcd01318a365ab4ff4b9dc7cbd63615138358
Phoomparin Mano <phoomparin@gmail.com>
2026-03-02 17:53:56 +0700

    starship: update shell

--- config/dot_config/fish/conf.d/3-prompt.fish
+++ config/dot_config/fish/conf.d/3-prompt.fish
@@ -1,2 +1,6 @@
+if type -q starship
+  starship init fish | source
+end
+
 # Tide prompt config
 set -gx tide_right_prompt_items status cmd_duration context jobs direnv node python rustc java php pulumi ruby go distrobox toolbox terraform aws nix_shell crystal elixir zig
