117c1026e95b2bcb2fef2f438eff63f53f48de98
Phoomparin Mano <phoomparin@gmail.com>
2026-03-02 17:23:59 +0700

    sync dotfiles

--- config/dot_config/nvchad/lua/configs/lspconfig.lua
+++ config/dot_config/nvchad/lua/configs/lspconfig.lua
@@ -1,7 +1,7 @@
 -- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
-local lspconfig = require "lspconfig"
+local lspconfig = vim.lsp.config
 
 -- EXAMPLE
 local servers = { "html", "cssls" }
@@ -9,12 +9,14 @@ local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
+  lspconfig[lsp] = {
     on_attach = nvlsp.on_attach,
     on_init = nvlsp.on_init,
     capabilities = nvlsp.capabilities,
   }
 end
+
+vim.lsp.enable(servers)
 
 -- configuring single server, example: typescript
 -- lspconfig.ts_ls.setup {
