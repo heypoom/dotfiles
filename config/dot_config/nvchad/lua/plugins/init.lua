b498794ac51b2d97984d15697a033159894dd05e
Phoomparin Mano <phoomparin@gmail.com>
2026-07-13 21:06:52 +0700

    feat(nvchad): add markdown rendering

--- config/dot_config/nvchad/lua/plugins/init.lua
+++ config/dot_config/nvchad/lua/plugins/init.lua
@@ -10,4 +10,27 @@ return {
       require "configs.lspconfig"
     end,
   },
+
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    opts = {
+      checkbox = {
+        custom = {
+          in_progress = { raw = "[.]", rendered = "⊡ ", highlight = "RenderMarkdownWarn" },
+        },
+      },
+    },
+  },
+
+  {
+    "epilande/checkbox-cycle.nvim",
+    ft = "markdown",
+    opts = {
+      states = { "[ ]", "[.]", "[x]" },
+    },
+    keys = {
+      { "<CR>", "<cmd>CheckboxCycleNext<cr>", ft = "markdown", desc = "Cycle checkbox" },
+      { "<S-CR>", "<cmd>CheckboxCyclePrev<cr>", ft = "markdown", desc = "Cycle checkbox (reverse)" },
+    },
+  },
 }
