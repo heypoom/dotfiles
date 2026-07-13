0d2644a4a2f00c321cfe4148110944d53efea13d
Phoomparin Mano <phoomparin@gmail.com>
2026-07-13 23:32:10 +0700

    feat(fish, yazi): add y shell wrapper

---
+++ config/dot_config/fish/functions/y.fish
@@ -0,0 +1,8 @@
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	command yazi $argv --cwd-file="$tmp"
+	if read -z cwd < "$tmp"; and [ "$cwd" != "$PWD" ]; and test -d "$cwd"
+		builtin cd -- "$cwd"
+	end
+	command rm -f -- "$tmp"
+end
