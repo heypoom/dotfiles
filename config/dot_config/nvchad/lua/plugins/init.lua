25fa2114a7a2301d806f95602c5ae4979c7ff6e6
Phoomparin Mano <phoomparin@gmail.com>
2026-07-13 21:54:40 +0700

    feat(nvchad): add flash.nvim

--- config/dot_config/nvchad/lua/plugins/init.lua
+++ config/dot_config/nvchad/lua/plugins/init.lua
@@ -36,6 +36,39 @@ return {
   },
 
   {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    opts = {
+      labels = "asdfghjklqwertyuiopzxcvbnm",
+      search = {
+        multi_window = true,
+        mode = "exact",
+        wrap = true,
+      },
+      jump = {
+        autojump = false,
+        jumplist = true,
+      },
+      modes = {
+        search = {
+          enabled = true,
+        },
+        char = {
+          enabled = true,
+          jump_labels = false,
+        },
+      },
+    },
+    keys = {
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<C-s>", mode = "c", function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    },
+  },
+
+  {
     "epilande/checkbox-cycle.nvim",
     ft = "markdown",
     opts = {
