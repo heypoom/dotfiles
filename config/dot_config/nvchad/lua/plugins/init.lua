ee0a2a9c66b68a3bd9b5897a697bf8dbf4dc5fdf
Phoomparin Mano <phoomparin@gmail.com>
2026-07-14 03:35:17 +0700

    fix(neovim): markdown injection crash

--- config/dot_config/nvchad/lua/plugins/init.lua
+++ config/dot_config/nvchad/lua/plugins/init.lua
@@ -14,6 +14,41 @@ return {
   {
     "MeanderingProgrammer/render-markdown.nvim",
     lazy = false,
+    init = function()
+      -- nvim-treesitter's markdown injection directive crashes on Neovim 0.12.4
+      -- for some fenced blocks. Use Neovim's simpler runtime query instead.
+      vim.treesitter.query.set(
+        "markdown",
+        "injections",
+        [[
+          (fenced_code_block
+            (info_string
+              (language) @injection.language)
+            (code_fence_content) @injection.content)
+
+          ((html_block) @injection.content
+            (#set! injection.language "html")
+            (#set! injection.combined)
+            (#set! injection.include-children))
+
+          ((minus_metadata) @injection.content
+            (#set! injection.language "yaml")
+            (#offset! @injection.content 1 0 -1 0)
+            (#set! injection.include-children))
+
+          ((plus_metadata) @injection.content
+            (#set! injection.language "toml")
+            (#offset! @injection.content 1 0 -1 0)
+            (#set! injection.include-children))
+
+          ([
+            (inline)
+            (pipe_table_cell)
+          ] @injection.content
+            (#set! injection.language "markdown_inline"))
+        ]]
+      )
+    end,
     opts = {
       checkbox = {
         unchecked = {
